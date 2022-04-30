00de60b0c414c8b541840349f5721cc7170dac90
Amrawy <ahmed.m.amrawy@gmail.com>
2022-04-30 23:28:55 +0200

    refactor(frontend): delete LinksForm to be replaced with the search component

--- frontend/src/components/LinksForm.js
+++
@@ -1,9 +0,0 @@
-const LinksForm = ({ inputLink, handleChangeLink }) => {
-  return (
-    <form>
-      <input value={inputLink} onChange={handleChangeLink}></input>
-    </form>
-  );
-};
-
-export default LinksForm;
